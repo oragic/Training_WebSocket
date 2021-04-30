297b76270b0baea8c659b8e74105a7f6a99e5007
Rai D Alves <raidouglas_alves@outlook.com>
2021-04-29 21:04:39 -0300

    message and user methods created

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,15 @@
+import { UsersService } from "../services/UsersService";
+import {  Request, Response } from "express"
+
+class UsersController{
+    async create(req:Request,res:Response){
+        const { email } = req.body
+        
+        const usersService = new UsersService()
+        const user = await usersService.create(email)
+
+        return res.json(user)
+    }
+}
+
+export { UsersController }
---
+++ src/services/UsersService.ts
@@ -0,0 +1,29 @@
+import { getCustomRepository } from "typeorm"
+import { UserRepository } from "../repositories/UsersRepository"
+
+class UsersService{
+    async create(email:string){
+
+        const userRepository = getCustomRepository(UserRepository)
+
+        const userExists = await userRepository.findOne({
+
+            email,
+
+        })
+        if(userExists){
+            return userExists
+        }
+        const user = userRepository.create({
+            email,
+        })
+
+        await userRepository.save(user)
+
+        return user
+
+        return
+    }
+}
+
+export { UsersService }
